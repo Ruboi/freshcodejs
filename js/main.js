0e4b3463a55b35351ccd6649fb3c318b532ffd54
ruboi <ruslan.boiko@smile-ukraine.com>
2023-11-21 12:54:48 +0200

    first commit - add files

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+
+
+// const inpValue1 = Number(prompt('insert num1'));
+// const inpValue2 = Number(prompt('insert num1'));
+
+// if (inpValue1 > inpValue2) {
+//     console.log('inpValue1 :>> ', inpValue1);
+// } else {
+//     console.log('inpValue2 :>> ', inpValue2);
+// }
+
+
+const date = 20;
+const friday = false;
+
+if (date%2===0 && friday) {
+    console.log('Hi');
+} else {
+    console.log('Sorry');
+}
+
+const userAge = 60;
+const userChildAge = 20;
+
+if (userAge >= 60 || userChildAge < 18) {
+    console.log('Hi');
+} else {
+    console.log('Sorry');
+}
